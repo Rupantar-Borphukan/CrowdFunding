505d43f1b754d2591a42c7dcc4e8c6246cfece5c
Rupantar Borphukan <94108574+Rupantar-Borphukan@users.noreply.github.com>
2023-07-14 04:53:22 +0530

    Update crowdfund.sol

--- crowdfund.sol
+++ crowdfund.sol
@@ -2,9 +2,8 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery{
 
-    
-
-
+    address payable[] public players;
+    address public manager;
 
     constructor(){
         manager = msg.sender;
